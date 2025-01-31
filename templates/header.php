3c6479a2e69dbffbe86318e14f75b52e891e44be
sharad68 <sharadbhusal49@gmail.com>
2025-01-31 14:27:09 +0100

    feat: header and footer setup-v2

--- templates/header.php
+++ templates/header.php
@@ -3,39 +3,39 @@
 use App\Entity\User;
 use App\Tools\NavigationTools;
 ?>
-<<!DOCTYPE html>
-    <html lang="fr">
+<!DOCTYPE html>
+<html lang="fr">
 
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-            crossorigin="anonymous" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
-        <link rel="stylesheet" href="./assets/override-bootstrap.css" />
-        <title>Okaz</title>
-    </head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <link rel="stylesheet" href="./assets/override-bootstrap.css" />
+    <title>Okaz</title>
+</head>
 
-    <body>
-        <div class="container">
-            <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-                <div class="col-md-3 mb-2 mb-md-0">
-                    <a href="index.html" class="d-inline-flex link-body-emphasis text-decoration-none">
-                        <img width="120" src="./assets/logo-okaz.png" alt="Logo Okaz" />
-                    </a>
-                </div>
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div class="col-md-3 mb-2 mb-md-0">
+                <a href="index.html" class="d-inline-flex link-body-emphasis text-decoration-none">
+                    <img width="120" src="./assets/logo-okaz.png" alt="Logo Okaz" />
+                </a>
+            </div>
 
-                <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="index.html" class="nav-link px-2 link-secondary">Accueil</a></li>
-                    <li><a href="annonces.html" class="nav-link px-2">Annonces</a></li>
-                </ul>
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="index.html" class="nav-link px-2 link-secondary">Accueil</a></li>
+                <li><a href="annonces.html" class="nav-link px-2">Annonces</a></li>
+            </ul>
 
-                <div class="col-md-3 text-end">
-                    <a class="btn btn-outline-primary me-2" href="#">Connexion</a>
-                    <a class="btn btn-primary" href="#">Inscription</a>
-                </div>
-            </header>
+            <div class="col-md-3 text-end">
+                <a class="btn btn-outline-primary me-2" href="#">Connexion</a>
+                <a class="btn btn-primary" href="#">Inscription</a>
+            </div>
+        </header>
