369ff7b2d87535d8f1a239a0eac815ac72fd30db
adhermit <maharjanraman456@gmail.com>
2025-02-05 10:51:24 +0100

    feat:CURD

--- App/Repository/AdsRepository.php
+++ App/Repository/AdsRepository.php
@@ -40,4 +40,29 @@ class AdsRepository extends Repository
             return false;
         }
     }
+
+    public function persist(Ads $ads)
+    {
+
+        if ($ads->getId() !== null) {
+            $query = $this->pdo->prepare(
+                'UPDATE ads SET title = :title, description = :description,  
+                                                    price = :price, image = :image  WHERE id = :id'
+            );
+            $query->bindValue(':id', $ads->getId(), $this->pdo::PARAM_INT);
+        } else {
+            $query = $this->pdo->prepare(
+                'INSERT INTO ads (title, description, price, image) 
+                                                    VALUES (:title, :description, :price, :image)'
+            );
+        }
+
+        $query->bindValue(':title', $ads->getTitle(), $this->pdo::PARAM_STR);
+        $query->bindValue(':description', $ads->getDescription(), $this->pdo::PARAM_STR);
+        $query->bindValue(':price', $ads->getPrice(), $this->pdo::PARAM_INT);
+        $query->bindValue(':image', $ads->getImage(), $this->pdo::PARAM_STR);
+
+
+        return $query->execute();
+    }
 }
